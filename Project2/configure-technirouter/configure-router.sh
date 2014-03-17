566e5d5b79fb4c8523ef8ddea49ccb15a3969584
Matthieu Baerts <matttbe@gmail.com>
2014-03-17 16:16:09 +0100

    Script: more user friendly (need to be tested...)

--- Project2/configure-technirouter/configure-router.sh
+++ Project2/configure-technirouter/configure-router.sh
@@ -5,13 +5,23 @@ if [ $# -gt 0 ]; then
 	dest=$1;
 fi
 
+sshcmd() {
+	echo "$@"
+	sshpass -p 'root' ssh root@$dest "$@"
+}
+
+scpcmd() {
+	echo "scp $@"
+	sshpass -p 'root' scp $@
+}
+
 ssh-keygen -R $dest
 echo "=== ACCEPT THE NEW KEY (pass: 'root') ==="
 echo "ssh root@$dest echo Hello"
 ssh root@$dest "echo Hello" || echo "ERROR"
 
 echo "scp packages/*.ipk root@$dest:/root"
-sshpass -p "root" scp packages/*.ipk root@$dest:/root
+scpcmd packages/*.ipk root@$dest:/root
 
 commands="opkg install *.ipk;iptables -F;iptables -X;iptables -P INPUT ACCEPT;iptables -P FORWARD ACCEPT;iptables -P OUTPUT ACCEPT;/etc/init.d/quagga start"
 set -- "$commands"
@@ -21,14 +31,12 @@ for item in ${Array[*]}; do
 	sshpass -p "root" ssh root@$dest "$item"
 done
 
-sshcmd() {
-	echo "$@"
-	sshpass -p 'root' ssh root@$dest "$@"
-}
-
+echo
+echo "Here is the list of routers"
 ls ../part1
-read -p "Which router?" RouterDir
-sshpass -p "root" scp -r ../part1/$RouterDir/* root@$dest:/
+read -p "Which one do you want? " RouterDir
+test -d ../part1/$RouterDir && echo "Copy in progress" || echo "Wrong dir"
+scpcmd -r ../part1/$RouterDir/* root@$dest:/
 
 
 sshcmd "sed -i '/^exit 0/ s/exit 0/\n\/etc\/init.d\/quagga start\n\nexit 0/g' /etc/rc.local"
