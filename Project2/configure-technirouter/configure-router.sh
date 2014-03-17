dce8c13e9d4827015bf02bf738885a4a2f351ccd
Matthieu Baerts <matttbe@gmail.com>
2014-03-17 14:51:42 +0100

    Project2: script: Fixed connexion bug

--- Project2/configure-technirouter/configure-router.sh
+++ Project2/configure-technirouter/configure-router.sh
@@ -1,16 +1,22 @@
+#!/bin/bash
+
 dest="192.168.1.1"
 if [ $# -gt 0 ]; then
 	dest=$1;
 fi
 
-echo "sshpass -p root scp packages/*.ipk root@$dest:/root"
+ssh-keygen -R $dest
+echo "=== ACCEPT THE NEW KEY (pass: 'root') ==="
+echo "ssh root@$dest echo Hello"
+ssh root@$dest "echo Hello" || echo "ERROR"
+
+echo "scp packages/*.ipk root@$dest:/root"
 sshpass -p "root" scp packages/*.ipk root@$dest:/root
 
-commands="opkg install *.ipk;iptables -F;iptables -X;iptables -P INPUT ACCEPT;iptables -P FORWARD ACCEPT;iptables -P OUTPUT ACCEPT" 
-set -- "$commands" 
-IFS=";"; declare -a Array=($*) 
+commands="opkg install *.ipk;iptables -F;iptables -X;iptables -P INPUT ACCEPT;iptables -P FORWARD ACCEPT;iptables -P OUTPUT ACCEPT;/etc/init.d/quagga start"
+set -- "$commands"
+IFS=";"; declare -a Array=($*)
 for item in ${Array[*]}; do
 	echo "sshpass -p root ssh root@$dest $item"
 	sshpass -p "root" ssh root@$dest "$item"
 done
-
