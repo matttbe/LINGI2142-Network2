67d57054dfcfda645dbe61b91d9a36975393aba8
Julien Colmonts <julien.colmonts@gmail.com>
2014-03-18 13:38:46 +0100

    Add rc.local to all etc dir - reboot at end of script

--- Project2/configure-technirouter/configure-router.sh
+++ Project2/configure-technirouter/configure-router.sh
@@ -23,13 +23,8 @@ ssh root@$dest "echo Hello" || echo "ERROR"
 echo "scp packages/*.ipk root@$dest:/root"
 scpcmd packages/*.ipk root@$dest:/root
 
-commands="opkg install *.ipk;iptables -F;iptables -X;iptables -P INPUT ACCEPT;iptables -P FORWARD ACCEPT;iptables -P OUTPUT ACCEPT;/etc/init.d/quagga start"
-set -- "$commands"
-IFS=";"; declare -a Array=($*)
-for item in ${Array[*]}; do
-	echo "ssh root@$dest $item"
-	sshpass -p "root" ssh root@$dest "$item"
-done
+sshcmd "opkg install *.ipk"
+
 
 echo
 echo "Here is the list of routers"
@@ -38,8 +33,5 @@ read -p "Which one do you want? " RouterDir
 test -d ../part1/$RouterDir && echo "Copy in progress" || echo "Wrong dir"
 scpcmd -r ../part1/$RouterDir/* root@$dest:/
 
-
-sshcmd "sed -i '/^exit 0/ s/exit 0/\n\/etc\/init.d\/quagga start\n\nexit 0/g' /etc/rc.local"
-
 echo "Install done - Restart the router"
-sshcmd "halt"
+sshcmd "reboot"
