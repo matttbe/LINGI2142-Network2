51db974e3a9b356932d4cecee0cf436ebbb0bf8c
Matthieu Baerts <matttbe@gmail.com>
2014-03-26 13:13:50 +0100

    Added a configure script for the part2 to modify some files to one specific router

---
+++ Project2/part2/configure.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+ROUTER_ID=$1
+ROUTER_DIR=$2
+DEFAULT=default
+
+echo "Copy files"
+## a new tmp dir
+rm -rf "$ROUTER_DIR"
+## copy networkfile
+cp -r "$ROUTER_ID" "$ROUTER_DIR"
+## copy files supported by the script
+cp -r "$DEFAULT"/* "$ROUTER_DIR"
+
+#################
+## Modif files ##
+#################
+
+## zebra.conf ##
+echo "zebra.conf"
+sed -i "s/@ROUTER_ID@/$ROUTER_ID/g" "$ROUTER_DIR/etc/quagga/zebra.conf"
+
+## ospf6d.conf ##
+echo "ospf6d.conf"
+INTERFACES=`grep "config interface 'eth" "$ROUTER_DIR/etc/config/network" | cut -d\' -f2`
+for INTF in $INTERFACES; do
+	echo "interface $INTF
+ ipv6 ospf6 cost 1
+ ipv6 ospf6 hello-interval 10
+ ipv6 ospf6 dead-interval 40
+ ipv6 ospf6 retransmit-interval 5
+ ipv6 ospf6 priority 1
+ ipv6 ospf6 transmit-delay 1
+ ipv6 ospf6 instance-id 0
+!
+!" >> "$ROUTER_DIR/etc/quagga/ospf6d.conf"
+done
+
+echo "router ospf6
+ router-id 255.1.1.$ROUTER_ID" >> "$ROUTER_DIR/etc/quagga/ospf6d.conf"
+
+for INTF in $INTERFACES; do
+	echo " interface $INTF area 0.0.0.0" >> "$ROUTER_DIR/etc/quagga/ospf6d.conf"
+done
+
+echo "!
+log file /etc/quagga/ospf6d.log
+!" >> "$ROUTER_DIR/etc/quagga/ospf6d.conf"
+
+touch "$ROUTER_DIR/etc/quagga/ospf6d.log"
